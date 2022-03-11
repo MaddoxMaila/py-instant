b1f9d1c741dffb5e2a6ad09ffb9f45eb1d05d03a
maddox <maddoxmaila719@gmail.com>
2022-03-11 21:43:35 +0200

    Added pyinstant class that will implement singleton design pattern

--- PyInstant.py
+++ PyInstant.py
@@ -1,1 +1,26 @@
+from typing import Type
 from redis import ConnectionPool
+
+# PyInstant Will Implement The Singleton Design Pattern
+# To Better Handle Connections To Redis
+
+class PyInstant:
+
+    __instance = None
+
+    def __init__(self) -> None:
+        
+        if PyInstant.__instance != None:
+            raise Exception("PyInstant is singleton")
+        else:
+            PyInstant.__instance = self
+
+    @staticmethod
+    def get_instance():
+
+        if PyInstant.__instance is None:
+            PyInstant()
+        return PyInstant.__instance
+
+    def connect(self, ) -> None:
+        pass
