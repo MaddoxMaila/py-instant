c16fd32e849b126fa28fe8e462553f2883d4be21
maddox <maddoxmaila719@gmail.com>
2022-03-13 22:49:14 +0200

    Added a method to get the connections from the the class, also connecting the Connection class with the PyInstant class

--- PyInstant.py
+++ PyInstant.py
@@ -1,7 +1,8 @@
 from typing import Type
 from redis import ConnectionPool, Redis
 
-from Exceptions import MissingSSLCert
+from .Exceptions import SingletonException
+from Connection import Connection
 
 # PyInstant Will Implement The Singleton Design Pattern
 # To Better Handle Connections To Redis
@@ -9,13 +10,15 @@ from Exceptions import MissingSSLCert
 class PyInstant:
 
     __instance = None
+    __connection = None
 
     def __init__(self) -> None:
         
         if PyInstant.__instance != None:
-            raise Exception("PyInstant is singleton")
+            raise SingletonException("This class PyInstant is a Singleton")
         else:
             PyInstant.__instance = self
+            PyInstant.__connection = Connection.get_instance()
 
     @staticmethod
     def get_instance():
@@ -25,6 +28,21 @@ class PyInstant:
         return PyInstant.__instance
 
     def create_connection(self, host: str, port: int, db: int = 0, ssl: bool = False, ssl_ca_certs: str = None) -> None:
-        pass
+        
+        PyInstant.__connection.load_connection_details(
+            host=host,
+            port=port,
+            db=db,
+            ssl=ssl,
+            ssl_ca_certs=ssl_ca_certs
+        ).connect()
+
+    def get_raw_connections(self) -> dict:
+        return {
+            "redis": PyInstant.__connection.get_redis_connection(),
+            "pool": PyInstant.__connection.get_redis_pool_connection()
+        }
+
+
 
 
