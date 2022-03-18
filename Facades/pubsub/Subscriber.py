b894fed5183bc8314b4e7b85f92c64d37afe3251
MaddoxMaila <maddoxmaila@gmail.com>
2022-03-18 23:05:26 +0200

    Implemented Subscriber functionality with more to come

--- Facades/pubsub/Subscriber.py
+++ Facades/pubsub/Subscriber.py
@@ -1,4 +1,30 @@
+from Exceptions import SingletonException
+from Connection import Connection
 
+class Subscriber(Connection):
+    
+    __instance = None
 
-class Subscriber:
-    pass
+    def __init__(self) -> None:
+        
+        self.pub_sub = super().get_instance().get_redis_connection().pubsub()
+
+        if Subscriber.__instance is not None:
+            raise SingletonException("This class Subscriber is a singleton")
+        else:
+            Subscriber.__instance = self
+    
+    @staticmethod
+    def get_instance(self):
+        if Subscriber.__instance is None:
+            Subscriber()
+        return Subscriber.__instance
+
+    def add_channel(self, channel_name: str) -> None:
+        self.pub_sub.subscribe(channel_name)
+
+    def add_channels(self, channel_names: list) -> None:
+        (self.add_channel(channel_name=channel_name) for channel_name in  channel_names)
+
+    def channels_exists(self, channel_name: str or list) -> bool:
+        pass
