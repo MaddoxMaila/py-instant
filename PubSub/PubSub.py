46e8c29e04d7a28788e9f0ff86e058e4b3a7bb0a
MaddoxMaila <maddoxmaila@gmail.com>
2022-03-18 23:21:59 +0200

    PubSub will implement/extract Subscriber & Publisher features for quick use

--- PubSub/PubSub.py
+++ PubSub/PubSub.py
@@ -1,6 +1,20 @@
-from Facades import Subscriber, Publisher
+import imp
+from ..Connection import Connection
+from ..Facades import Subscriber
+from ..Exceptions import ChannelTypeException
+from redis.client import PubSub
 
-class PubSub(Subscriber, Publisher):
+class PubSub(Connection):
     
     def __init__(self) -> None:
-        super().__init__()
+
+        self.redis_connection = super().get_instance()
+
+    def subscribe_channels(self, channel_name: str or list) -> None:
+        
+        if type(channel_name) is 'str':
+            Subscriber.get_instance().add_channel(channel_name=channel_name)
+        elif type(channel_name) is 'list':
+            Subscriber.get_instance().add_channels(channel_names=channel_name)
+        else:
+            raise ChannelTypeException("Wrong type for a channel name")
