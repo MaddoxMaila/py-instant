e0fa2b4223b1692bfb323c582e3e53475f7f3b75
MaddoxMaila <maddoxmaila@gmail.com>
2022-03-19 18:54:41 +0200

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from ensurepip import version
+from setuptools import setup
+
+setup(
+    name='pyinstant',
+    version='0.0.1',
+    description="A real time data streaming and caching library built on top of Redis",
+    url="https://github.com/MaddoxMaila/py-instant",
+    author="Tshepang Maddox Maila",
+    author_email="maila.tshepang719@gmail.com",
+    license="MIT",
+    packages=['PyInstant'],
+    zip_safe=False
+)
