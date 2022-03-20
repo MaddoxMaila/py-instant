4426f1ac3a9526c78eb189ee47ef1ff8b2af5ae1
MaddoxMaila <maddoxmaila@gmail.com>
2022-03-20 23:18:59 +0200

    added Cache module

--- PyInstant/Cache/Cache.py
+++ PyInstant/Cache/Cache.py
@@ -1,3 +1,19 @@
+from ..Exceptions import SingletonException
 
 class Cache:
-    pass
+    
+    __instance = None
+
+    def __init__(self) -> None:
+        
+        if Cache.__instance != None:
+            raise SingletonException("This class Cache is a Singleton")
+        else:
+            Cache.__instance = self
+    
+    @staticmethod
+    def get_instance():
+        if Cache.__instance is None:
+            Cache()
+        return Cache.__instance
+        
