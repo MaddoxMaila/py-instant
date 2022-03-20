2f5d9fab2dfce784a61743e9b41bf3a8ef52c27c
MaddoxMaila <maddoxmaila@gmail.com>
2022-03-20 23:18:32 +0200

    Added PubSub related exceptions

---
+++ PyInstant/Exceptions/PubSubExceptions.py
@@ -0,0 +1,2 @@
+class UnregisteredChannelException(Exception):
+    pass
--- PyInstant/Exceptions/__init__.py
+++ PyInstant/Exceptions/__init__.py
@@ -2,3 +2,4 @@ from .TypeExceptions import *
 from .MissingSSLCertException import *
 from .SingletonException import *
 from .ConnectionException import *
+from .PubSubExceptions import *
