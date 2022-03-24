0f37bbad6d31cc72a2ee0cc5c0536c17ab165b37
MaddoxMaila <maddoxmaila719@gmail.com>
2022-03-24 10:14:46 +0200

    Made cacher a singleton... might make all other facades singletons

--- PyInstant/Facades/cache/Cacher.py
+++ PyInstant/Facades/cache/Cacher.py
@@ -1,11 +1,27 @@
+from ...Exceptions import SingletonException
+from ...Connection import Connection
 
 
+class Cacher(Connection):
 
-class Cacher:
+    __instance = None
 
     def __init__(self) -> None:
-        pass
+        
+        self.redis = super().get_instance().get_redis_connection()
+
+        if Cacher.__instance != None:
+            raise SingletonException("This class Cacher is a Singleton")
+        else:
+            Cacher.__instance = self
+    
+    @staticmethod
+    def get_instance():
+        if Cacher.__instance is None:
+            Cacher()
+        return Cacher.__instance
 
 
     def make(self, key: str) -> None:
-        pass
+        
+        # check if key exists in all data type storage fields
